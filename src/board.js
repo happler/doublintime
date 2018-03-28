9c01df1eeb51a35ff65c5e46e980b4e8e6507e39
Happler <harry.appler@gmail.com>
2018-03-28 19:18:22 -0400

    fix splash positioning and experiment with border

--- src/board.js
+++ src/board.js
@@ -1,14 +1,14 @@
-import { zip, reverse, unzip, isEqual, cloneDeep } from 'lodash';
+import { zip, reverse, unzip, isEqual, cloneDeep } from "lodash";
 
-class Board{
-  constructor(ctx, size = 4){
+class Board {
+  constructor(ctx, size = 4) {
     this.size = size;
     this.ctx = ctx;
     this.grid = this.makeGrid(size);
     this.potScore = 0;
     this.score = 0;
-    this.scoreDOM = document.getElementsByClassName('score')[0];
-    this.DIRS = ['up', 'down', 'left', 'right'];
+    this.scoreDOM = document.getElementsByClassName("score")[0];
+    this.DIRS = ["up", "down", "left", "right"];
     this.draw = this.draw.bind(this);
     this.gameOver = this.gameOver.bind(this);
     this.draw();
@@ -17,12 +17,11 @@ class Board{
     // this.addFixedCell();
   }
 
-  makeGrid(size){
+  makeGrid(size) {
     const grid = [];
-    for(let row = 0; row < size; row++){
-
+    for (let row = 0; row < size; row++) {
       const newRow = [];
-      for(let col = 0; col < size; col++){
+      for (let col = 0; col < size; col++) {
         newRow.push(0);
       }
       grid.push(newRow);
@@ -30,10 +29,9 @@ class Board{
     return grid;
   }
 
-
-  addRandomCell(empties){
+  addRandomCell(empties) {
     const randomCell = empties[Math.floor(Math.random() * empties.length)];
-    const val = Math.random() > .8 ? 4 : 2;
+    const val = Math.random() > 0.8 ? 4 : 2;
     this.grid[randomCell[0]][randomCell[1]] = val;
     setTimeout(() => {
       this.drawNewSquare(randomCell[0], randomCell[1], val, 1);
@@ -45,19 +43,14 @@ class Board{
             this.drawNewSquare(randomCell[0], randomCell[1], val, 4);
             setTimeout(() => {
               this.drawNewSquare(randomCell[0], randomCell[1], val, 5);
-
             }, 25);
-
           }, 25);
-
         }, 25);
-
       }, 25);
-
-    }, 25 );
+    }, 25);
   }
 
-  addFixedCell(){
+  addFixedCell() {
     this.grid[0][0] = 0;
     this.grid[0][1] = 2;
     this.grid[0][2] = 2;
@@ -75,53 +68,56 @@ class Board{
     this.grid[3][2] = 2;
     this.grid[3][3] = 2;
     this.draw();
-
   }
 
-  collapse(arr){
+  collapse(arr) {
     const newArr = [];
     const resultArr = [];
 
     arr.forEach(el => {
-      if (el) {newArr.push(el);}
+      if (el) {
+        newArr.push(el);
+      }
     });
 
-    for(let i = 0; i < newArr.length; i++){
-      if (newArr[i] === newArr[i + 1]){
+    for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i] === newArr[i + 1]) {
         resultArr.push(newArr[i] * 2);
-        this.potScore += (newArr[i] * 2);
+        this.potScore += newArr[i] * 2;
         i++;
-      } else{
+      } else {
         resultArr.push(newArr[i]);
       }
     }
 
-    while(resultArr.length < this.size){ resultArr.push(0); }
+    while (resultArr.length < this.size) {
+      resultArr.push(0);
+    }
     return resultArr;
   }
 
-  potMove(dir){
+  potMove(dir) {
     let modArr = cloneDeep(this.grid);
     let collArr;
     switch (dir) {
-      case 'up':
+      case "up":
         modArr = zip(...modArr);
         collArr = modArr.map(tRow => this.collapse(tRow));
         collArr = unzip(collArr);
         break;
-      case 'down':
+      case "down":
         modArr = zip(...modArr);
         modArr = modArr.map(row => reverse(row));
         collArr = modArr.map(tRow => this.collapse(tRow));
         collArr = collArr.map(row => reverse(row));
         collArr = unzip(collArr);
         break;
-      case 'right':
+      case "right":
         modArr = modArr.map(row => reverse(row));
         collArr = modArr.map(tRow => this.collapse(tRow));
-        collArr = collArr.map(row =>reverse(row));
+        collArr = collArr.map(row => reverse(row));
         break;
-      case 'left':
+      case "left":
         collArr = modArr.map(row => this.collapse(row));
         break;
       default:
@@ -130,36 +126,35 @@ class Board{
     return collArr;
   }
 
-  makeMove(dir){
+  makeMove(dir) {
     const postMove = this.potMove(dir);
-    if(!isEqual(postMove, this.grid)){
+    if (!isEqual(postMove, this.grid)) {
       this.score += this.potScore;
       this.potScore = 0;
       this.grid = postMove;
       this.draw();
       const empties = this.emptySpaces();
-      if(empties.length){
+      if (empties.length) {
         this.addRandomCell(empties);
-        if(empties.length === 1 && this.isOver()){
+        if (empties.length === 1 && this.isOver()) {
           setTimeout(this.gameOver, 250);
         }
       }
     }
   }
 
-  gameOver(){
-    const splash = document.getElementsByClassName('game-over-splash')[0];
-    const finalScore = document.getElementById('final-score');
-    splash.classList.add('shown');
+  gameOver() {
+    const splash = document.getElementsByClassName("game-over-splash")[0];
+    const finalScore = document.getElementById("final-score");
+    splash.classList.add("shown");
     finalScore.innerHTML = this.score;
-
   }
 
-  emptySpaces(){
+  emptySpaces() {
     const empties = [];
-    for(let row = 0; row < this.size; row++){
-      for(let col = 0; col < this.size; col++){
-        if(!this.grid[row][col]){
+    for (let row = 0; row < this.size; row++) {
+      for (let col = 0; col < this.size; col++) {
+        if (!this.grid[row][col]) {
           empties.push([row, col]);
         }
       }
@@ -167,10 +162,9 @@ class Board{
     return empties;
   }
 
-  isOver(){
-    for(let i = 0; i < this.DIRS.length; i++){
-
-      if(!isEqual(this.potMove(this.DIRS[i]), this.grid)){
+  isOver() {
+    for (let i = 0; i < this.DIRS.length; i++) {
+      if (!isEqual(this.potMove(this.DIRS[i]), this.grid)) {
         this.potScore = 0;
         return false;
       }
@@ -179,50 +173,65 @@ class Board{
     return true;
   }
 
-  drawNewSquare(row, col, val, pass){
-    this.ctx.fillStyle = val === 2 ? '#EDE4DB' : '#EBE0CB';
-    this.ctx.fillRect( ((120) * col) + 20 + (40 - 10 * (pass - 1)), ((120) * row) + 20 + (40 - 10 * (pass - 1)), 20 * pass, 20 * pass );
-    this.ctx.font = '30px sans-serif';
-    this.ctx.textAlign = 'center';
-    this.ctx.fillStyle = '#756E66';
-    if (pass > 1 ){
-      this.ctx.fillText(val, ((120) * col) + 70, ((120) * row) + 80);
+  drawNewSquare(row, col, val, pass) {
+    this.ctx.fillStyle = val === 2 ? "#EDE4DB" : "#EBE0CB";
+    this.ctx.fillRect(
+      120 * col + 20 + (40 - 10 * (pass - 1)),
+      120 * row + 20 + (40 - 10 * (pass - 1)),
+      20 * pass,
+      20 * pass
+    );
+    // Below code provides a slight border on new square spawning, current testing likes it without, but leaving for inspiration
+    // if (pass < 5) {
+    //   this.ctx.lineWidth = 5;
+    //   this.ctx.strokeStyle = "#A40E4C";
+    //   this.ctx.strokeRect(
+    //     120 * col + 20 + (40 - 10 * (pass - 1)),
+    //     120 * row + 20 + (40 - 10 * (pass - 1)),
+    //     20 * pass,
+    //     20 * pass
+    //   );
+    // }
+    this.ctx.font = "30px sans-serif";
+    this.ctx.textAlign = "center";
+    this.ctx.fillStyle = "#756E66";
+    if (pass > 1) {
+      this.ctx.fillText(val, 120 * col + 70, 120 * row + 80);
     }
   }
 
-  draw(){
+  draw() {
     this.scoreDOM.innerHTML = this.score;
-    this.ctx.fillStyle = '#B9ADA1';
+    this.ctx.fillStyle = "#B9ADA1";
     this.ctx.fillRect(0, 0, 500, 500);
-    const colors ={
-      0:'#CBC1B5',
-      2:'#EDE4DB',
-      4:'#EBE0CB',
-      8:'#EAB381',
-      16:'#E9996C',
-      32:'#E88266',
-      64:'#E66747',
-      128:'#ECD590',
-      256:'#EACA74',
-      512:'#EAC568',
-      1024:'#E8C15A',
-      2048:'#EABF51',
+    const colors = {
+      0: "#CBC1B5",
+      2: "#EDE4DB",
+      4: "#EBE0CB",
+      8: "#EAB381",
+      16: "#E9996C",
+      32: "#E88266",
+      64: "#E66747",
+      128: "#ECD590",
+      256: "#EACA74",
+      512: "#EAC568",
+      1024: "#E8C15A",
+      2048: "#EABF51"
     };
-    for(let i = 0; i < this.size; i++){
-      for(let j = 0; j < this.size; j++){
+    for (let i = 0; i < this.size; i++) {
+      for (let j = 0; j < this.size; j++) {
         const val = this.grid[i][j];
         this.ctx.fillStyle = colors[val];
-        this.ctx.fillRect( ((120) * j) + 20, ((120) * i) + 20, 100, 100 );
-        if (val){
-          this.ctx.font = '30px sans-serif';
-          this.ctx.textAlign = 'center';
-          if(val < 8){
-            this.ctx.fillStyle = '#756E66';
+        this.ctx.fillRect(120 * j + 20, 120 * i + 20, 100, 100);
+        if (val) {
+          this.ctx.font = "30px sans-serif";
+          this.ctx.textAlign = "center";
+          if (val < 8) {
+            this.ctx.fillStyle = "#756E66";
           } else {
-          this.ctx.fillStyle = '#FFFFFF';
+            this.ctx.fillStyle = "#FFFFFF";
           }
-          this.ctx.fillText(val, ((120) * j) + 70, ((120) * i) + 80);
-
+          this.ctx.fillText(val, 120 * j + 70, 120 * i + 80);
         }
       }
     }
