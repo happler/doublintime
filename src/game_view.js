65dca7d4539832a5094cef96bb971cb2099b0c21
Happler <harry.appler@gmail.com>
2018-04-09 18:28:47 -0400

    touch control working, but firing multiple times per move

--- src/game_view.js
+++ src/game_view.js
@@ -1,55 +1,91 @@
-import Game from './game';
+import Game from "./game";
 
-
-class GameView{
-  constructor(game, ctx){
-    this.ctx = ctx;
+class GameView {
+  constructor(game, ctx, canvasEl) {
     this.game = game;
+    this.ctx = ctx;
+    this.canvasEl = canvasEl;
     this.resetGame = this.resetGame.bind(this);
-    this.newButton = document.getElementsByClassName('new-game-button')[0];
-    this.resetButton = document.getElementsByClassName('play-again-button')[0];
-    this.splash = document.getElementsByClassName('game-over-splash')[0];
-
+    this.newButton = document.getElementsByClassName("new-game-button")[0];
+    this.resetButton = document.getElementsByClassName("play-again-button")[0];
+    this.splash = document.getElementsByClassName("game-over-splash")[0];
+    this.lockChangeAlert = this.lockChangeAlert.bind(this);
+    this.updatePosition = this.updatePosition.bind(this);
   }
 
-  resetGame(){
+  resetGame() {
     this.game = new Game(this.ctx);
     this.start();
   }
 
-  bindKeyHandlers(){
-    key('w, up', 'all',  () =>{
-        this.game.board.makeMove('up');
-      });
-    key('a, left', 'all', () =>{
-        this.game.board.makeMove('left');
-      });
-    key('s, down', 'all', () =>{
-        this.game.board.makeMove('down');
-      });
-    key('d, right', 'all', () =>{
-        this.game.board.makeMove('right');
-      });
-    this.newButton.addEventListener('click', this.resetGame);
-    this.resetButton.addEventListener('click', this.resetGame);
-
+  lockChangeAlert() {
+    if (
+      document.pointerLockElement === this.canvas ||
+      document.mozPointerLockElement === this.canvas
+    ) {
+      console.log("The pointer lock status is now locked");
+      document.addEventListener("mousemove", this.updatePosition, false);
+    } else {
+      console.log("The pointer lock status is now unlocked");
+      document.removeEventListener("mousemove", this.updatePosition, false);
+    }
   }
 
-  removeKeyHandlers(){
-    this.newButton.removeEventListener('click', this.resetGame);
-    this.resetButton.removeEventListener('click', this.resetGame);
-    key.deleteScope('all');
+  updatePosition(e) {
+    const deltaX = e.movementX;
+    const deltaY = e.movementY;
+    console.log(`deltaX =>${deltaX}, deltaY =>${deltaY}`);
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+      if (Math.sign(deltaX) === 1) {
+        console.log("right");
+        this.game.board.makeMove("right");
+      } else {
+        console.log("left");
+        this.game.board.makeMove("left");
+      }
+    } else {
+      if (Math.sign(deltaY) === 1) {
+        console.log("down");
+        this.game.board.makeMove("down");
+      } else {
+        console.log("up");
+        this.game.board.makeMove("up");
+      }
     }
+  }
 
+  bindKeyHandlers() {
+    key("w, up", "all", () => {
+      this.game.board.makeMove("up");
+    });
+    key("a, left", "all", () => {
+      this.game.board.makeMove("left");
+    });
+    key("s, down", "all", () => {
+      this.game.board.makeMove("down");
+    });
+    key("d, right", "all", () => {
+      this.game.board.makeMove("right");
+    });
+    this.newButton.addEventListener("click", this.resetGame);
+    this.resetButton.addEventListener("click", this.resetGame);
+    this.canvasEl.addEventListener("click", () =>
+      this.canvasEl.requestPointerLock()
+    );
+    document.addEventListener("pointerlockchange", this.lockChangeAlert, false);
+  }
 
+  removeKeyHandlers() {
+    this.newButton.removeEventListener("click", this.resetGame);
+    this.resetButton.removeEventListener("click", this.resetGame);
+    key.deleteScope("all");
+  }
 
-  start(){
+  start() {
     this.removeKeyHandlers();
     this.bindKeyHandlers();
-    this.splash.classList.remove('shown');
-
+    this.splash.classList.remove("shown");
   }
-
 }
 
 export default GameView;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-import Board from './board';
-import Game from './game';
-import GameView from './game_view';
+import Board from "./board";
+import Game from "./game";
+import GameView from "./game_view";
 
-
-
-document.addEventListener('DOMContentLoaded', () =>{
-  const canvasEl = document.getElementsByTagName('canvas')[0];
+document.addEventListener("DOMContentLoaded", () => {
+  const canvasEl = document.getElementsByTagName("canvas")[0];
   canvasEl.width = 500;
   canvasEl.height = 500;
 
-  const ctx = canvasEl.getContext('2d');
+  const ctx = canvasEl.getContext("2d");
   const game = new Game(ctx);
-  const gameView = new GameView(game, ctx);
+  const gameView = new GameView(game, ctx, canvasEl);
   gameView.start();
 });
